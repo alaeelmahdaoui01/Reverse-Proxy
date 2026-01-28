9bb9420a5221b9d4e608e39d06b9064ea18a2c73
El Mahdaoui Alae <alaeelmahdaoui01@gmail.com>
2026-01-28 21:06:25 +0100

    added AdminApi. Also i need to fix some issues

--- proxy/AdminApi.go
+++ proxy/AdminApi.go
@@ -1,4 +1,128 @@
 package proxy
 
 
-//  to doooooo 
+import (
+	"encoding/json"
+	"net/http"
+	"log"
+) 
+
+
+type AdminApi struct{
+	SPool *ServerPool
+}
+
+func NewAdminApi(sPool *ServerPool) *AdminApi {
+	return &AdminApi{SPool: sPool}
+}
+
+type ResponseStructure struct {
+	TotalBackends    int   `json:"total_backends"`
+	ActiveBackends     int      `json:"active_backends"`
+	Backends []*Backend   `json:"backends"`
+}
+
+func (api *AdminApi) StatusHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return 
+	} 
+	api.SPool.mux.Lock()
+	defer api.SPool.mux.Unlock()
+
+	active_backends := 0 
+	for _ ,backend := range api.SPool.Backends {
+		if backend.IsAlive() {
+			active_backends++
+		}
+	}
+
+
+	response := ResponseStructure{
+		TotalBackends : len(api.SPool.Backends),
+		ActiveBackends: active_backends,
+		Backends: api.SPool.Backends,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+type BackendReq struct {
+	Url string `json:"url"`
+}
+
+// the r.Body contains the url to add and to delete (we will tranform it from json to the struct)
+func (api *AdminApi) BackendsHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+		case http.MethodPost:
+
+			var req BackendReq
+			api.SPool.mux.Lock()
+			defer api.SPool.mux.Unlock()
+			error := json.NewDecoder(r.Body).Decode(&req)
+			if error != nil {
+				http.Error(w, "Invalid Json" , http.StatusBadRequest)
+				return
+			}
+
+			backend, err := NewBackend(req.Url)
+			if err!=nil {
+				http.Error(w, "invalid backend URL", http.StatusBadRequest)
+				return
+			}
+
+			api.SPool.AddBackend(backend)
+			w.WriteHeader(http.StatusCreated)
+
+
+
+		case http.MethodDelete:
+			var req BackendReq
+
+			api.SPool.mux.Lock()
+			defer api.SPool.mux.Unlock()
+
+			error := json.NewDecoder(r.Body).Decode(&req)
+			if error != nil {
+				http.Error(w, "Invalid Json" , http.StatusBadRequest)
+				return
+			}
+
+			for idx, backend := range api.SPool.Backends {
+				if backend.URL.String() == req.Url{
+					api.SPool.Backends = append(api.SPool.Backends[:idx],api.SPool.Backends[idx+1:]...,)
+					w.WriteHeader(http.StatusOK)
+					return 
+				}
+				
+			}
+
+			http.Error(w, "backend not found", http.StatusNotFound)
+
+		
+		default:
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (api *AdminApi) StartAdminServer() {
+	http.HandleFunc("/backends", api.BackendsHandler)
+	http.HandleFunc("/status", api.StatusHandler)
+	log.Println("Admin server running on :8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+
+// func main() {
+// 	if p := os.Getenv("PORT"); p != "" {
+// 		port = p
+// 	}
+
+// 	http.HandleFunc("/students", studentsHandler)
+// 	http.HandleFunc("/health", healthHandler)
+
+// 	log.Printf("Backend running on :%s\n", port)
+// 	log.Fatal(http.ListenAndServe(":"+port, nil))
+// }
--- proxy/main/main.go
+++ proxy/main/main.go
@@ -39,6 +39,8 @@ func main() {
 	// flag.Parse()
 	// cfg, err := proxy.LoadConfig(*configPath)
 
+	
+
     cfg, err := proxy.LoadConfig("../../config.json" )
     if err != nil {
         log.Fatal(err)
@@ -53,6 +55,10 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
+
+	// running admin api in goroutine and the healthchecker 
+	admin := proxy.NewAdminApi(pool)
+	go admin.StartAdminServer()
 
 	healthChecker := proxy.NewHealthChecker(pool, cfg.HealthCheckFreq)
 	go healthChecker.Start()
@@ -83,6 +89,14 @@ func main() {
     ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
     defer cancel()
 
+	// i have an error i think here, i need to shutdown hta adminapi server ? 
+	// exemple : 2026/01/27 21:33:43 Proxy listening on :9000
+	// 2026/01/27 21:33:43 Admin server running on :8081 
+	// 2026/01/27 21:34:13 Backend at  http://localhost:8001 up! ...
+	// 2026/01/27 21:40:41 Shutting down proxy...
+	// 2026/01/27 21:40:43 Backend at  http://localhost:8003 up! ...
+	// 2026/01/27 21:40:51 Shutdown failed: context deadline exceeded
+	// exit status 1
     if err := srv.Shutdown(ctx); err != nil {
         log.Fatalf("Shutdown failed: %v", err)
     }
@@ -104,9 +118,33 @@ func main() {
 // then run the main.go 
 
 //  then check proxy response with curl : curl.exe http://localhost:9000/students
+//  to run the adminapi get/status : curl http://localhost:8081/status
+//  to add backend dynamically : curl -X POST http://localhost:8081/backends \
+//  -H "Content-Type: application/json" \
+//  -d '{"url":"http://localhost:8004"}'
+//  to remove backend : curl -X DELETE http://localhost:8081/backends \
+//  -H "Content-Type: application/json" \
+//  -d '{"url":"http://localhost:8002"}'
+
+//  i check proxy again with curl http://localhost:9000/students
+
+
 
 
 
 
 // What this main.go does so far 
 // Load config, Build server pool, Create load balancer, Create proxy handler, Start HTTP server
+
+
+
+// Como les 3 e,dhom current connections : 0
+
+// PS C:\Users\HP\Documents\reverse proxy prjct> curl.exe http://localhost:8081/status
+// {"total_backends":3,"active_backends":3,"backends":[{"url":{"Scheme":"http","Opaque":"","User":null,"Host":"localhost:8001","Path":"","RawPath":"","OmitHost":false,"ForceQuery":false,"RawQuery":"","Fragment":"","RawFragment":""},"alive":true,"current_connections":0},{"url":{"Scheme":"http","Opaque":"","User":null,"Host":"localhost:8002","Path":"","RawPath":"","OmitHost":false,"ForceQuery":false,"RawQuery":"","Fragment":"","RawFragment":""},"alive":true,"current_connections":0},{"url":{"Scheme":"http","Opaque":"","User":null,"Host":"localhost:8003","Path":"","RawPath":"","OmitHost":false,"ForceQuery":false,"RawQuery":"","Fragment":"","RawFragment":""},"alive":true,"current_connections":0}]}
+// ASLAN ELACH HADCHI TWIL BZF 
+
+// ALSO WHYYYY : PS C:\Users\HP\Documents\reverse proxy prjct> curl.exe -X POST http://localhost:8081/backends -H "Content-Type: application/json" -d "{\"url\":\"http://localhost:8004\"}"
+// >>
+// Invalid Json
+// curl: (3) URL rejected: Port number was not a decimal number between 0 and 65535
